db07ff6a4a8a614701f9005b05e10ec996b9c8ff
zelinskiy <zelinskiy1917@gmail.com>
2017-11-19 15:14:03 +0000

    lab3

--- lab3/c/main.c
+++ lab3/c/main.c
@@ -4,12 +4,17 @@
 #include <math.h>
 #include <cpuid.h>
 #include <string.h>
-#include "mmintrin.h"
+#include <mmintrin.h>
+#include <emmintrin.h> 
 
 //https://software.intel.com/sites/landingpage/IntrinsicsGuide
 
+//TODO:
+// more simd for sqrt
+// task 8 (complex multiplication)
+
 void task1(){
-     printf("\nTASK 1\n\n");
+     printf("TASK 1\n");
      
      int eax, ebx, ecx, edx;
      char vendor[13];
@@ -105,47 +110,287 @@ void task2_d(double* a, double* b, double* c, int n){
 }
 
 void task2(){
-     int n = 1024;
+     printf("TASK 2\n");
+     int n = 4096 * 4096;
      struct timespec start, end;
 
-     // 8
-     int8_t* a = malloc(n*sizeof(int8_t));
-     int8_t* b = malloc(n*sizeof(int8_t));
-     int8_t* c = malloc(n*sizeof(int8_t));
+     // 8i
+     int8_t* a8 = malloc(n*sizeof(int8_t));
+     int8_t* b8 = malloc(n*sizeof(int8_t));
+     int8_t* c8 = malloc(n*sizeof(int8_t));
 
      for(int i = 0; i < n; i++){
-	  a[i] = rand();
-	  b[i] = rand();
+	  a8[i] = rand();
+	  b8[i] = rand();
+     }     
+     
+     clock_gettime(CLOCK_REALTIME, &start);
+     task2_i8(a8, b8, c8, n);
+     clock_gettime(CLOCK_REALTIME, &end);     
+     printf("8 bit in %fs\n", (end.tv_sec - start.tv_sec)
+	    + (end.tv_nsec - start.tv_nsec) / 1e9);
+
+     free(a8);free(b8);free(c8);
+
+     // 16i
+     int16_t* a16 = malloc(n*sizeof(int16_t));
+     int16_t* b16 = malloc(n*sizeof(int16_t));
+     int16_t* c16 = malloc(n*sizeof(int16_t));
+
+     for(int i = 0; i < n; i++){
+	  a16[i] = rand();
+	  b16[i] = rand();
      }
      
      clock_gettime(CLOCK_REALTIME, &start);
-     printf("8 bit: %30.32f\n",
-	    integrate(fun1, 0.0, 1.0, 1.0/(double) n));
-     clock_gettime(CLOCK_REALTIME, &end);
-     
-     printf("in %fs\n", (end.tv_sec - start.tv_sec)
+     task2_i16(a16, b16, c16, n);
+     clock_gettime(CLOCK_REALTIME, &end);     
+     printf("16 bit in %fs\n", (end.tv_sec - start.tv_sec)
 	    + (end.tv_nsec - start.tv_nsec) / 1e9);
+
+     free(a16);free(b16);free(c16);
+
+     // 32i
+     int32_t* a32 = malloc(n*sizeof(int32_t));
+     int32_t* b32 = malloc(n*sizeof(int32_t));
+     int32_t* c32 = malloc(n*sizeof(int32_t));
+
+     for(int i = 0; i < n; i++){
+	  a32[i] = rand();
+	  b32[i] = rand();
+     }
+     
+     clock_gettime(CLOCK_REALTIME, &start);
+     task2_i32(a32, b32, c32, n);
+     clock_gettime(CLOCK_REALTIME, &end);     
+     printf("32 bit in %fs\n", (end.tv_sec - start.tv_sec)
+	    + (end.tv_nsec - start.tv_nsec) / 1e9);
+
+     free(a32);free(b32);free(c32);
+
+     // 64i
+     int64_t* a64 = malloc(n*sizeof(int64_t));
+     int64_t* b64 = malloc(n*sizeof(int64_t));
+     int64_t* c64 = malloc(n*sizeof(int64_t));
+
+     for(int i = 0; i < n; i++){
+	  a64[i] = rand();
+	  b64[i] = rand();
+     }
+     
+     clock_gettime(CLOCK_REALTIME, &start);
+     task2_i64(a64, b64, c64, n);
+     clock_gettime(CLOCK_REALTIME, &end);     
+     printf("64 bit in %fs\n", (end.tv_sec - start.tv_sec)
+	    + (end.tv_nsec - start.tv_nsec) / 1e9);
+
+     free(a64);free(b64);free(c64);
+
+     // float
+     float* af = malloc(n*sizeof(float));
+     float* bf = malloc(n*sizeof(float));
+     float* cf = malloc(n*sizeof(float));
+
+     float alpha_f = -100000.0;
+     float beta_f = 100000.0;
+
+     for(int i = 0; i < n; i++){
+	  af[i] = alpha_f + ((float)rand()/(float)RAND_MAX)*(beta_f-alpha_f);
+	  bf[i] = alpha_f + ((float)rand()/(float)RAND_MAX)*(beta_f-alpha_f);
+     }
+     
+     clock_gettime(CLOCK_REALTIME, &start);
+     task2_f(af, bf, cf, n);
+     clock_gettime(CLOCK_REALTIME, &end);     
+     printf("float in %fs\n", (end.tv_sec - start.tv_sec)
+	    + (end.tv_nsec - start.tv_nsec) / 1e9);
+
+     free(af);free(bf);free(cf);
+
+     // double
+     double* ad = malloc(n*sizeof(double));
+     double* bd = malloc(n*sizeof(double));
+     double* cd = malloc(n*sizeof(double));
+
+     double alpha_d = -100000.0;
+     double beta_d = 100000.0;
+
+     for(int i = 0; i < n; i++){
+	  ad[i] = alpha_d + ((double)rand()/(double)RAND_MAX)*(beta_d-alpha_d);
+	  bd[i] = alpha_d + ((double)rand()/(double)RAND_MAX)*(beta_d-alpha_d);	  
+     }
+     
+     clock_gettime(CLOCK_REALTIME, &start);
+     task2_d(ad, bd, cd, n);
+     clock_gettime(CLOCK_REALTIME, &end);     
+     printf("double in %fs\n", (end.tv_sec - start.tv_sec)
+	    + (end.tv_nsec - start.tv_nsec) / 1e9);
+
+     free(ad);free(bd);free(cd);
 }
+
+// task 4
+// 4096 * 4096
+// +--------+--------------+
+// |  type  |     time     |
+// +--------+--------------+
+// |  int8  |   0.047516s  |
+// +--------+--------------+
+// |  int16 |   0.053025s  |
+// +--------+--------------+
+// |  int32 |   0.065553s  |
+// +--------+--------------+
+// |  int64 |   0.120089s  |
+// +--------+--------------+
+// |  float |   0.061225s  |
+// +--------+--------------+
+// | double |   0.115424s  |
+// +--------+--------------+
+
+void print_array_float(float* a, int n){
+     for(int i = 0; i<n; i++){
+	  printf("%f ", a[i]);
+     }
+     printf("\n");	 
+}
+
+void arrays_equal_float(float* a, float* b, int n){     
+     for(int i = 0; i<n; i++){
+	  if(a[i] != b[i]) {
+	       printf("%f != %f at %d\n ", a[i], b[i], i);
+	       return;
+	  }	  
+     }
+     printf("arrays equal\n");	 
+}
+
+void sqrt_float(
+     float* x,
+     float* y,
+     int n){
+     for (int i = 0; i < n; i++){
+	  y[i] = sqrtf(x[i]);
+     }
+}
+
+void sqrt_float_sse(
+     float* x,
+     float* y,
+     int n){
+     __m128* x_sse = (__m128*)x;
+     __m128* y_sse = (__m128*)y;
+     int r = sizeof(__m128)/sizeof(float);
+     
+     for (int i = 0; i < (n/r); i++){
+	  y_sse[i] = _mm_sqrt_ps(x_sse[i]);
+     }
+     for(int i = n - (n % r); i < n; i++){
+	  y[i] = sqrt(x[i]);
+     }	  
+     
+     y = (float*)y_sse;
+}
+
+void sqrt_double(
+     double* x,
+     double* y,
+     int n){
+     for (int i = 0; i < n; i++){
+	  y[i] = sqrt(x[i]);
+     }
+}
+
+void sqrt_double_sse2(
+     double* x,
+     double* y,
+     int n){
+     __m128d* x_sse2 = (__m128d*)x;
+     __m128d* y_sse2 = (__m128d*)y;
+     int r = sizeof(__m128d)/sizeof(double);
+     
+     for (int i = 0; i < (n/r); i++){
+	  y_sse2[i] = _mm_sqrt_pd(x_sse2[i]);
+     }
+     for(int i = n - (n % r); i < n; i++){
+	  y[i] = sqrt(x[i]);
+     }	  
+     
+     y = (double*)y_sse2;
+}
+
+void task6(){
+     printf("TASK 6\n");
+     int n = 4096 * 4096;
+     struct timespec start, end;
+
+     // float
+     float* x_f = malloc(n*sizeof(float));
+     float* y_f = malloc(n*sizeof(float));
+
+     float alpha_f = -1000000.0;
+     float beta_f = 1000000.0;
+
+     for(int i = 0; i < n; i++){
+	  x_f[i] = alpha_f + ((float)rand()/(float)RAND_MAX)*(beta_f-alpha_f);
+     }
+     
+     clock_gettime(CLOCK_REALTIME, &start);
+     sqrt_float(x_f, y_f, n);
+     clock_gettime(CLOCK_REALTIME, &end);     
+     printf("float no simd in %fs\n", (end.tv_sec - start.tv_sec)
+	    + (end.tv_nsec - start.tv_nsec) / 1e9);
+
+     clock_gettime(CLOCK_REALTIME, &start);
+     sqrt_float_sse(x_f, y_f, n);
+     clock_gettime(CLOCK_REALTIME, &end);     
+     printf("float sse in %fs\n", (end.tv_sec - start.tv_sec)
+	    + (end.tv_nsec - start.tv_nsec) / 1e9);
+
+     // double
+     double* x_d = malloc(n*sizeof(double));
+     double* y_d = malloc(n*sizeof(double));
+
+     double alpha_d = -1000000.0;
+     double beta_d = 1000000.0;
+
+     for(int i = 0; i < n; i++){
+	  x_d[i] = alpha_d + ((double)rand()/(double)RAND_MAX)*(beta_d-alpha_d);
+     }
+     
+     clock_gettime(CLOCK_REALTIME, &start);
+     sqrt_double(x_d, y_d, n);
+     clock_gettime(CLOCK_REALTIME, &end);     
+     printf("double no simd in %fs\n", (end.tv_sec - start.tv_sec)
+	    + (end.tv_nsec - start.tv_nsec) / 1e9);
+
+     clock_gettime(CLOCK_REALTIME, &start);
+     sqrt_double_sse2(x_d, y_d, n);
+     clock_gettime(CLOCK_REALTIME, &end);     
+     printf("double sse2 in %fs\n", (end.tv_sec - start.tv_sec)
+	    + (end.tv_nsec - start.tv_nsec) / 1e9);
+
+}
+
+// task 7
+// 4096 * 4096
+// +--------------+--------------+
+// |      type    |     time     |
+// +--------------+--------------+
+// |      float   |  0.322759s   |
+// +--------------+--------------+
+// |   sse float  |  0.031860s   |
+// +--------------+--------------+
+// |     double   |  0.366414s   |
+// +--------------+--------------+
+// | sse2 double  |  0.072700s   |
+// +--------------+--------------+
 
 
 int main (int argc, char *argv[]) {
      srand(time(NULL));
      printf("BEGIN\n");
-     //task1();
-
-     
-     int8_t* a = malloc(n*sizeof(int8_t));
-     int8_t* b = malloc(n*sizeof(int8_t));
-     int8_t* c = malloc(n*sizeof(int8_t));
-
-     for(int i = 0; i < n; i++){
-	  a[i] = rand();
-	  b[i] = rand();
-     }
-     task2_i8(a, b, c, n);
-     for(int i = 0; i < n; i++){
-	  printf("%i ", c[i]);
-     }
-     
+     task1();
+     task2();
+     task6();
      printf("\nEND");
 }
